8b1a74388c4839d3f3d91c4f7af7ef798ea01311
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-02-13 18:08:35 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+		for(int n=1; n<101; n++) {
+		cout << n + 1 << endl;
+		cout << endl;
+		cout << endl;
+	}
+	cout << "Fizz" << endl;
+	cout << "Buzz" << endl;
+	cout << "FizzBuzz" << endl;
+	
+	return 0;
+}
